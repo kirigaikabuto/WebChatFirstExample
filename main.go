653c66df1cd1e017b95a58adb9fceec38eb33a97
Yerassyl Tleugazy <tleugazy98@gmail.com>
2022-02-17 14:19:25 +0600

    commits

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	log.Print("Server starting at localhost:4444")
+	if err := http.ListenAndServe(":4444", nil); err != nil {
+		log.Fatal(err)
+	}
 }
