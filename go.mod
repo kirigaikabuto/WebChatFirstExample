4475378eb0022294812728b211b8311582017f27
Yerassyl Tleugazy <tleugazy98@gmail.com>
2022-02-17 14:20:10 +0600

    commits

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kirigaikabuto/WebChatFirstExample
 
 go 1.13
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
--- test_example.go
+++ test_example.go
@@ -1,41 +1,41 @@
 package main
-
-import "fmt"
-
-func one(c1 chan string) {
-	for i := 0; i < 5; i++ {
-		c1 <- "one"
-	}
-	close(c1)
-}
-
-func two(c2 chan string) {
-	for i := 0; i < 5; i++ {
-		c2 <- "two"
-	}
-	close(c2)
-}
-
-func main() {
-	c1 := make(chan string)
-	c2 := make(chan string)
-	go one(c1)
-	go two(c2)
-	for {
-		select {
-		case msg, ok := <-c1:
-			fmt.Println(msg)
-			if !ok {
-				c1 = nil
-			}
-		case msg, ok := <-c2:
-			fmt.Println(msg)
-			if !ok {
-				c2 = nil
-			}
-		}
-		if c1 == nil && c2 == nil {
-			break
-		}
-	}
-}
+//
+//import "fmt"
+//
+//func one(c1 chan string) {
+//	for i := 0; i < 5; i++ {
+//		c1 <- "one"
+//	}
+//	close(c1)
+//}
+//
+//func two(c2 chan string) {
+//	for i := 0; i < 5; i++ {
+//		c2 <- "two"
+//	}
+//	close(c2)
+//}
+//
+//func main() {
+//	c1 := make(chan string)
+//	c2 := make(chan string)
+//	go one(c1)
+//	go two(c2)
+//	for {
+//		select {
+//		case msg, ok := <-c1:
+//			fmt.Println(msg)
+//			if !ok {
+//				c1 = nil
+//			}
+//		case msg, ok := <-c2:
+//			fmt.Println(msg)
+//			if !ok {
+//				c2 = nil
+//			}
+//		}
+//		if c1 == nil && c2 == nil {
+//			break
+//		}
+//	}
+//}
