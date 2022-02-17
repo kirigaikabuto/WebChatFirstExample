631c0b186987ba0d12214915a3e0877ee00afcd1
Yerassyl Tleugazy <tleugazy98@gmail.com>
2022-02-17 14:20:37 +0600

    commits

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 	log.Print("Server starting at localhost:4444")
-	if err := http.ListenAndServe(":4444", nil); err != nil {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
