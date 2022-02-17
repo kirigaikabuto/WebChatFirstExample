3d4bc669e5f28f7cb48c277dfd53abf0ab518bd5
Yerassyl Tleugazy <tleugazy98@gmail.com>
2022-02-17 14:23:14 +0600

    commits

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kirigaikabuto/WebChatFirstExample
 go 1.13
 
 require (
+	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/joho/godotenv v1.4.0
 )
--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"github.com/gomodule/redigo/redis"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
 )
 
+var (
+	rdb *redis.Client
+)
+
 func main() {
+	redisURL := os.Getenv("REDIS_URL")
+	opt, err := redis.ParseURL(redisURL)
+	if err != nil {
+		panic(err)
+	}
+	rdb = redis.NewClient(opt)
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 	log.Print("Server starting at localhost:4444")
-	err := godotenv.Load()
+	err = godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
